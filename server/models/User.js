f25eceec12f6df249304cecf5639b017bac1a6f7
davjdo <davido_204@hotmail.com>
2018-11-11 19:54:33 +0100

    add Login routewith comparePassword and generateToken methods

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const SALT_I = 10;
+require('dotenv').config();
 
 const userSchema = mongoose.Schema({
 	email: {
@@ -56,6 +58,23 @@ userSchema.pre('save', function(next) {
 		next();
 	}
 });
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+	bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+		if (err) return cb(err);
+		return cb(null, isMatch);
+	});
+};
+
+userSchema.methods.generateToken = function(cb) {
+	var user = this;
+	var token = jwt.sign(user._id.toHexString(), process.env.SECRET);
+	user.token = token;
+	user.save(function(err, user) {
+		if (err) return cb(err);
+		cb(null, user);
+	});
+};
 
 const User = mongoose.model('User', userSchema);
 
--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -18,4 +18,35 @@ router.post('/register', (req, res) => {
 	});
 });
 
+// @route   POST api/users/login
+// @desc    Log a user
+// @access  Public
+router.post('/login', (req, res) => {
+	User.findOne({ email: req.body.email }, (err, user) => {
+		if (!user) {
+			return res.json({
+				loginSuccess: false,
+				message: 'Auth failed, email not found'
+			});
+		}
+		user.comparePassword(req.body.password, (err, isMatch) => {
+			if (!isMatch) {
+				return res.json({
+					loginSuccess: false,
+					message: 'Wrong password'
+				});
+			}
+			user.generateToken((err, user) => {
+				if (err) return res.status(400).send(err);
+				res
+					.cookie('w_auth', user.token)
+					.status(200)
+					.json({
+						loginSuccess: true
+					});
+			});
+		});
+	});
+});
+
 module.exports = router;
