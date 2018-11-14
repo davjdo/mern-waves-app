51c951e851f6e71bfc6b2163269f679ff7212934
davjdo <davido_204@hotmail.com>
2018-11-14 19:39:46 +0100

    add RegisterLogin component and MyButton reusable button component

---
+++ client/src/components/Register_Login/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import MyButton from '../utils/Button';
+
+const RegisterLogin = () => {
+	return (
+		<div className="page_wrapper">
+			<div className="container">
+				<div className="register_login_container">
+					<div className="left">
+						<h1>New Customers</h1>
+						<p>
+							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias
+							dolorem corporis voluptatum nihil asperiores sed saepe unde rem,
+							possimus ab optio ut dolores officia accusamus fugit voluptas
+							beatae sapiente ipsa.
+						</p>
+						<MyButton
+							type="default"
+							title="Create an account"
+							linkTo="/register"
+							addStyles={{
+								margin: '10px 0 0 0'
+							}}
+						/>
+					</div>
+					<div className="right">
+						<h2>Registered Customers</h2>
+						<p>If you have an account, please log in.</p>
+						LOGIN
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default RegisterLogin;
--- client/src/routes.js
+++ client/src/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Layout from './hoc/Layout';
 import Home from './components/Home';
+import RegisterLogin from './components/Register_Login';
 
 const Routes = () => {
 	return (
 		<Layout>
 			<Switch>
+				<Route exact path="/register_login" component={RegisterLogin} />
 				<Route exact path="/" component={Home} />
 			</Switch>
 		</Layout>
