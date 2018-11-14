df74f0332017fdf05367ea323f2e15e1917275dc
davjdo <davido_204@hotmail.com>
2018-11-14 01:06:24 +0100

    add Routes, Layout, Header and Footer components using react-router-dom and react-fontawesome

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Routes from './routes';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = (
+	<Router>
+		<Routes />
+	</Router>
+);
 
+ReactDOM.render(app, document.getElementById('root'));
