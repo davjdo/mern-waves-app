3bd5625c9b48f597b104ee3923b4b91c9f702921
davjdo <davido_204@hotmail.com>
2018-11-16 02:58:50 +0100

    add UserLayout hoc component and UserDashboard component and route

--- client/src/routes.js
+++ client/src/routes.js
@@ -4,11 +4,13 @@ import Layout from './hoc/Layout';
 import Home from './components/Home';
 import RegisterLogin from './components/Register_Login';
 import Register from './components/Register_Login/Register';
+import UserDashboard from './components/User';
 
 const Routes = () => {
 	return (
 		<Layout>
 			<Switch>
+				<Route exact path="/user/dashboard" component={UserDashboard} />
 				<Route exact path="/register" component={Register} />
 				<Route exact path="/register_login" component={RegisterLogin} />
 				<Route exact path="/" component={Home} />
