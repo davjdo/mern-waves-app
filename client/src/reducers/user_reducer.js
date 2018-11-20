f0f9856d3f6dba52b9535c62afc3f7b4af37f042
davjdo <davido_204@hotmail.com>
2018-11-20 23:12:49 +0100

    add preventing routes with auth action and reducer

--- client/src/reducers/user_reducer.js
+++ client/src/reducers/user_reducer.js
@@ -1,4 +1,4 @@
-import { LOGIN_USER, REGISTER_USER } from '../actions/types';
+import { LOGIN_USER, REGISTER_USER, AUTH_USER } from '../actions/types';
 
 export default function(state = {}, action) {
 	switch (action.type) {
@@ -11,6 +11,11 @@ export default function(state = {}, action) {
 			return {
 				...state,
 				registerSuccess: action.payload
+			};
+		case AUTH_USER:
+			return {
+				...state,
+				userData: action.payload
 			};
 		default:
 			return state;
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Auth from './hoc/Auth';
 import Layout from './hoc/Layout';
 import Home from './components/Home';
 import RegisterLogin from './components/Register_Login';
@@ -10,10 +11,18 @@ const Routes = () => {
 	return (
 		<Layout>
 			<Switch>
-				<Route exact path="/user/dashboard" component={UserDashboard} />
-				<Route exact path="/register" component={Register} />
-				<Route exact path="/register_login" component={RegisterLogin} />
-				<Route exact path="/" component={Home} />
+				<Route
+					exact
+					path="/user/dashboard"
+					component={Auth(UserDashboard, true)}
+				/>
+				<Route exact path="/register" component={Auth(Register, false)} />
+				<Route
+					exact
+					path="/register_login"
+					component={Auth(RegisterLogin, false)}
+				/>
+				<Route exact path="/" component={Auth(Home, null)} />
 			</Switch>
 		</Layout>
 	);
